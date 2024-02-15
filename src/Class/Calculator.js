1cb681fbfcd803a94e54a3e27eea054673ff2a70
mr2yesung <mryesung@naver.com>
2024-02-16 06:42:37 +0900

    create calculator class and attach class method to element by event listener

---
+++ src/Class/Calculator.js
@@ -0,0 +1,69 @@
+class Calculator {
+  constructor(previousDisplayElement, currentDisplayElement) {
+    this.previousDisplayElement = previousDisplayElement;
+    this.currentDisplayElement = currentDisplayElement;
+    this.allClear();
+  }
+
+  allClear() {
+    this.previousDisplay = "";
+    this.currentDisplay = "";
+    this.operation = "";
+  }
+
+  delete() {
+    this.currentDisplay = this.currentDisplay.toString().slice(0, -1);
+  }
+
+  appendNumber(number) {
+    const currentDisplayString = this.currentDisplay.toString();
+    if (number === "." && currentDisplayString.includes(".")) return;
+    this.currentDisplay = this.currentDisplay + number.toString();
+  }
+
+  selectOperation(operation) {
+    if (!this.currentDisplay) return;
+    if (this.previousDisplay) this.calculate();
+    this.operation = operation;
+    this.previousDisplay = this.currentDisplay;
+    this.currentDisplay = "";
+  }
+
+  calculate() {
+    const prevNumber = parseFloat(this.previousDisplay);
+    const currNumber = parseFloat(this.currentDisplay);
+    if (isNaN(prevNumber) || isNaN(currNumber)) return;
+
+    let computeResult = 0;
+    switch (this.operation) {
+      case "+":
+        computeResult = prevNumber + currNumber;
+        break;
+      case "-":
+        computeResult = prevNumber - currNumber;
+        break;
+      case "*":
+        computeResult = prevNumber * currNumber;
+        break;
+      case "/":
+        computeResult = prevNumber / currNumber;
+        break;
+      default:
+        return;
+    }
+
+    this.operation = "";
+    this.currentDisplay = computeResult.toString();
+    this.previousDisplay = "";
+  }
+
+  updateDisplay() {
+    this.currentDisplayElement.innerText = this.currentDisplay;
+
+    this.previousDisplayElement.innerText = this.operation
+      ? `${this.previousDisplay} ${this.operation}`
+      : "";
+  }
+}
+
+export { Calculator };
--- src/main.js
+++ src/main.js
@@ -1,1 +1,41 @@
 import "./style.css";
+import { Calculator } from "./Class/Calculator";
+
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const deleteButton = document.querySelector("[data-delete]");
+const equalsButton = document.querySelector("[data-equals]");
+
+const previousDisplayElement = document.getElementById("previous-display");
+const currentDisplayElement = document.getElementById("current-display");
+
+const calculator = new Calculator(
+  previousDisplayElement,
+  currentDisplayElement
+);
+
+numberButtons.forEach((button) => {
+  addCalculatorEventListener(button, () => {
+    calculator.appendNumber(button.innerText);
+  });
+});
+
+operationButtons.forEach((button) => {
+  addCalculatorEventListener(button, () => {
+    calculator.selectOperation(button.innerText);
+  });
+});
+
+addCalculatorEventListener(allClearButton, () => calculator.allClear());
+
+addCalculatorEventListener(deleteButton, () => calculator.delete());
+
+addCalculatorEventListener(equalsButton, () => calculator.calculate());
+
+function addCalculatorEventListener(button, callback) {
+  button.addEventListener("click", () => {
+    callback();
+    calculator.updateDisplay();
+  });
+}
